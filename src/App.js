b860ea9e941e23a96ef88888f62e019bd8e0ed7f
ahnoldmoon <munnt72@hotmail.com>
2019-04-28 11:27:38 +0900

    #4-2. Practicing this.setState() : Complete

--- src/App.js
+++ src/App.js
@@ -16,52 +16,57 @@ import Movie from './Movie';
 //   "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
 // ]
 
-const movies = [
-  {
-    title: "matrix",
-    image: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
-  },
-  {
-    title: "Full Metal Jacket",
-    image: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg",
-  },
-  {
-    title: "Oldboy",
-    image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  },
-  {
-    title: "Star Wars",
-    image: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg",
-  }
-]
 
 class App extends Component {
   state = {
-    greeting: "Hello!"
+    greeting: "Hello!",
+    movies : [
+      {
+        title: "matrix",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+      },
+      {
+        title: "Full Metal Jacket",
+        image: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg",
+      },
+      {
+        title: "Oldboy",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+      },
+      {
+        title: "Star Wars",
+        image: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg",
+      }
+    ]
   }
   componentWillMount(){
-    console.log('Will Mount');
+    // console.log('Will Mount');
   }
 
   componentDidMount(){
-    console.log('Did Mount');
     setTimeout(() => {
-      // this.state.greeting = 'Hello again!'
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Trainspotting",
+            image: "https://secure.i.telegraph.co.uk/multimedia/archive/02809/TRAINSPOTTING_2809300b.jpg"
+          },
+          // ...this.state.movies
+        ]
       })
     }, 3000)
   }
 
   render(){
-    console.log('did render');
+    // console.log('did render');
     return(
       <div className="App">
         {/* {movies.map(movie => {
 	        return <Movie title={movie.title} image={movie.image} />
         })} */}
-        {this.state.greeting}
-        {movies.map((movie,index) => {
+        {/* {this.state.greeting} */}
+        {this.state.movies.map((movie,index) => {
           return <Movie title={movie.title} image={movie.image} key={index} />
         })}
       </div>
